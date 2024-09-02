e1356270af29c1e57dfed9a1f802b5a25084916b
Levi Luiz <leviluizmodesto@gmail.com>
2024-09-02 13:59:23 -0300

    Inicial Commit

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    loadGiftCards();
-    openTab('Sevira1'); // Abre a aba "Sevira1" por padrão
-});
-
 function openTab(tabName) {
     const tabs = document.getElementsByClassName('tab-content');
     for (let i = 0; i < tabs.length; i++) {
